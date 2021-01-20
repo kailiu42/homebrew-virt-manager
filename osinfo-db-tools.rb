ee8aa5e303bba130e896ee3a93944f62095e049b
Kai Liu <kai.liu@suse.com>
2021-01-20 22:59:47 +0800

    Downgrade osinfo-db-tools back to 1.6.0.

--- osinfo-db-tools.rb
+++ osinfo-db-tools.rb
@@ -1,30 +1,27 @@
 class OsinfoDbTools < Formula
   desc "Tools for managing the libosinfo database files"
   homepage "https://libosinfo.org"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.8.0.tar.xz"
-  sha256 "e9811a936e74dc262ce297ea7b8addeb111c782de500c9e285d3cdd5fe1b180d"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.6.0.tar.gz"
+  sha256 "d0d5b1196d73a7abed051be48d0e5b4aa196aac4cdbf8ddf52f57c0c492b2574"
 
   depends_on "pkg-config" => :build
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
 
   depends_on "gettext"
   depends_on "glib"
   depends_on "json-glib"
   depends_on "libarchive" # need >= 3.0.0
   depends_on "libsoup"
-  depends_on "libxml2"
-  depends_on "libxslt"
 
   def install
-    args = %w[
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --localstatedir=#{var}
+      --sysconfdir=#{etc}
     ]
-
-    mkdir "build" do
-      system "meson", *std_meson_args, ".."
-      system "ninja", "-v"
-      system "ninja", "install", "-v"
-    end
+    system "./configure", *args
+    system "make", "install"
   end
 
   test do
