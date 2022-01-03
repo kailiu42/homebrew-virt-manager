4157a1378a563b5b775039fb9f96f3f45c4c0de3
Kai Liu <kraml.liu@gmail.com>
2022-01-03 13:34:23 +0800

    osinfo-db-tools: fix libsoup dependency

--- osinfo-db-tools.rb
+++ osinfo-db-tools.rb
@@ -1,7 +1,8 @@
 class OsinfoDbTools < Formula
   desc "Tools for managing the libosinfo database files"
   homepage "https://libosinfo.org"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.9.0.tar.xz"
+  version "1.9.0"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-#{version}.tar.xz"
   sha256 "255f1c878bacec70c3020ff5a9cb0f6bd861ca0009f24608df5ef6f62d5243c0"
 
   depends_on "pkg-config" => :build
@@ -12,7 +13,7 @@ class OsinfoDbTools < Formula
   depends_on "glib"
   depends_on "json-glib"
   depends_on "libarchive" # need >= 3.0.0
-  depends_on "libsoup"
+  depends_on "libsoup@2"
 
   def install
     args = std_meson_args + %W[
