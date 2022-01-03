39fe7229efcf24f68fad6d1df3e176188947eae2
Kai Liu <kraml.liu@gmail.com>
2022-01-03 13:25:59 +0800

    osinfo-db: upgrade to v20211216

--- osinfo-db.rb
+++ osinfo-db.rb
@@ -1,9 +1,9 @@
 class OsinfoDb < Formula
   desc "Libosinfo database files"
   homepage "https://libosinfo.org/"
-  version "20210426"
+  version "20211216"
   url "https://releases.pagure.org/libosinfo/osinfo-db-#{version}.tar.xz"
-  sha256 "66c93b4a5b319b2ff7d40360d9e94486a9a45f2c0dd1c30d4b31de4f223070d0"
+  sha256 "09ece7b0e521330e31d12a59171e34f097181a7cbbcd6f8cd49f928923bb1082"
 
   depends_on "osinfo-db-tools" => :build
 
