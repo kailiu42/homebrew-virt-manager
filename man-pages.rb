010457e23f31df196ebb664fddb3c75edee0bdeb
Kai Liu <kraml.liu@gmail.com>
2022-05-01 17:02:20 +0800

    man-page: fix install failure

--- man-pages.rb
+++ man-pages.rb
@@ -5,6 +5,8 @@ class ManPages < Formula
   url "https://mirrors.edge.kernel.org/pub/linux/docs/man-pages/man-pages-#{version}.tar.xz"
   sha256 "614dae3efe7dfd480986763a2a2a8179215032a5a4526c0be5e899a25f096b8b"
 
+  depends_on "coreutils" => :build
+
   resource "man-pages-posix" do
     url "https://mirrors.edge.kernel.org/pub/linux/docs/man-pages/man-pages-posix/man-pages-posix-2017-a.tar.xz"
     sha256 "ce67bb25b5048b20dad772e405a83f4bc70faf051afa289361c81f9660318bc3"
@@ -13,9 +15,9 @@ class ManPages < Formula
   keg_only "Some files conflict with files from other formula, such as perl."
 
   def install
-    system "make", "install", "prefix=#{prefix}"
+    system "make", "install", "prefix=#{prefix}", "INSTALL=ginstall"
     resource("man-pages-posix").stage {
-      system "make", "install", "prefix=#{prefix}"
+      system "make", "install", "prefix=#{prefix}", "INSTALL=ginstall"
     }
   end
 end
