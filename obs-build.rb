25594279ee9d6be76c98511b55156c9604b5090b
Kai Liu <kraml.liu@gmail.com>
2023-03-11 18:47:52 +0800

    obs-build: upgrade to version 20230228

--- obs-build.rb
+++ obs-build.rb
@@ -1,9 +1,9 @@
 class ObsBuild < Formula
   desc "A Script to Build SUSE Linux RPMs"
-  version "1.4.1"
+  version "20230228"
   homepage "https://www.github.com/openSUSE/obs-build"
-  url "https://github.com/openSUSE/obs-build/archive/refs/tags/20210120.tar.gz"
-  sha256 "070c493720490d8bee9d43dfac673027a7a06485cd0d2f0ab9be2105fe3b8584"
+  url "https://github.com/openSUSE/obs-build/archive/refs/tags/#{version}.tar.gz"
+  sha256 "be126b4dbe32c207ca586df1d6685f8cd1d5afe948bd1e0f636373521c7529a0"
 
   def caveats
     "This formula needs $HOMEBREW_TEMP to be set to a case sensitive filesystem!"
