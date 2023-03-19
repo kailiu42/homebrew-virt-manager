ffa89a8839aa6e723da627b122862a5c23b0bd15
Kai Liu <kraml.liu@gmail.com>
2023-03-19 10:17:03 +0800

    osc: upgrade to version 1.0.1
    Signed-off-by: Kai Liu <kraml.liu@gmail.com>

--- osc.rb
+++ osc.rb
@@ -3,21 +3,30 @@ class Osc < Formula
 
   desc "Command-line interface to work with an Open Build Service"
   homepage "https://openbuildservice.org"
-  version "1.0.0b5"
+  version "1.0.1"
   url "https://github.com/openSUSE/osc/archive/refs/tags/#{version}.tar.gz"
-  sha256 "6a299e66c45abdda5c17349ef7eed75df5cbad87e0b5624bc2fce02562318533"
+  sha256 "c8bba251b1f546fef9a9b0739c6b74df9ecca4ffd832eb9351b651c6c0033755"
   license "GPL-2.0-or-later"
   head "https://github.com/openSUSE/osc.git", branch: "master"
 
-  depends_on "rust" => :build
+  depends_on "rust" => :build # for cryptography
+  depends_on "cffi"
   depends_on "openssl@3"
-  depends_on "python@3.10"
+  depends_on "pycparser"
+  depends_on "python@3.11"
+
+  uses_from_macos "curl"
 
   uses_from_macos "curl"
 
   resource "cryptography" do
     url "https://files.pythonhosted.org/packages/fa/f3/f4b8c175ea9a1de650b0085858059050b7953a93d66c97ed89b93b232996/cryptography-39.0.2.tar.gz"
     sha256 "bc5b871e977c8ee5a1bbc42fa8d19bcc08baf0c51cbf1586b0e87a2694dde42f"
+  end
+
+  resource "rpm" do
+    url "https://files.pythonhosted.org/packages/8c/15/ef9b3d4a0b4b9afe62fd2be374003643ea03fc8026930646ad0781bb9492/rpm-0.1.0.tar.gz"
+    sha256 "0e320a806fb61c3980c0cd0c5f5faec97c73c347432902ba2955a08a7b1a034f"
   end
 
   resource "urllib3" do
@@ -67,7 +76,7 @@ class Osc < Formula
       --url=https://github.com/openSUSE/osc/
     ]
 
-    system "/usr/local/Cellar/osc/#{version}/libexec/bin/argparse-manpage", *args
+    system "#{libexec}/bin/argparse-manpage", *args
 
     # Install man page
     man1.install "osc.1"
